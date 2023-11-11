31c5b87b5fe7af843f62b683935af6f6c2d0d40e
Weiwei <linweiwei2222@163.com>
2023-11-10 19:13:30 -0500

    homwork

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import products from './data'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import products from "./data";
 
 function App() {
-  console.log(products)
-  // add state in here
+  const [productList, setProductList] = useState(products);
+  // const [value, setValue] = useState("");
+  const [form, setForm] = useState({
+    itemName: "",
+    price: 0,
+    description: "Describe this item",
+  });
+  const [isHiring, setIsHiring] = useState(true);
 
-  // handleChange function
+  const handleChange = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  };
 
-// handleSubmit
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // do what you want with the form data
+    console.log("form in submit");
+    console.log(form);
+    const newProduct = form;
+    setProductList([newProduct, ...productList]);
+    setForm({
+      itemName: "",
+      price: 0,
+      description: "Describe this item",
+    });
+  };
+  const toggleHiring = () => {
+    setIsHiring(!isHiring);
+  };
 
-
-
-  // return
   return (
-
     <div>
-      <h1> Hi there! </h1>
+      <h1 onClick={toggleHiring}> Big Time Shopping </h1>{" "}
+      {isHiring ? (
+        <h2>Yes, we are hiring </h2>
+      ) : (
+        <h2>Sorry, try again tomorrow</h2>
+      )}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={form.itemName}
+          onChange={handleChange}
+          name="itemName"
+          placeholder="write name here"
+        />
+        <input
+          type="number"
+          value={form.price}
+          onChange={handleChange}
+          name="price"
+          placeholder="write price here"
+        />
+        <input
+          type="description"
+          value={form.description}
+          onChange={handleChange}
+          name="description"
+          placeholder="describe the product"
+        />
+        <input type="submit" value="Submit Form" />
+      </form>
+      <ul>
+        <div>
+          <h2>Preview our new item</h2> <h3>{form.itemName}</h3>
+          <h4>{form.price}</h4>
+          <h5>{form.description}</h5>
+        </div>
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App;
